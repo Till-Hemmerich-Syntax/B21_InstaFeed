c66040d6bf869b6754c805a287c3f7b4ade4f9a1
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2024-11-01 09:07:42 +0100

    Initial Commit

---
+++ B21_InstaFeed/B21_InstaFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  B21_InstaFeedApp.swift
+//  B21_InstaFeed
+//
+//  Created by Till Hemmerich on 01.11.24.
+//
+
+import SwiftUI
+
+@main
+struct B21_InstaFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
